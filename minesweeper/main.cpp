bf75e272991077b9cb2d0badcddcd4d762cf6c12
Sami Hatna <sami.hatna2003@gmail.com>
2022-04-07 17:07:17 +0100

    Initial Commit

---
+++ minesweeper/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  minesweeper
+//
+//  Created by Sami Hatna on 07/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
